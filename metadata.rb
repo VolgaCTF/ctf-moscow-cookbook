a9bd5fc2a72e9b199638322408d7d27c91e3b839
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2019-01-11 21:16:27 +0400

    Update dependencies

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name 'ctf-moscow'
 description 'Installs and configures ctf.moscow'
-version '1.3.1'
+version '1.4.0'
 
 recipe 'ctf-moscow', 'Installs and configures ctf.moscow'
 depends 'nodejs'
 depends 'nginx'
-depends 'tls', '~> 3.0.3'
+depends 'tls', '~> 3.1.0'
 depends 'instance', '~> 2.0.0'
